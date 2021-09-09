524c159b1b584736152322c204f6fc3bcf93e872
Artem <artemyipeshkov@gmail.com>
2021-09-09 18:53:07 +0300

    New way of comparing strings

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,8 +6,14 @@ import java.lang.Integer.max
    return File(filename).readLines()
 }*/
 
-fun lcsLength(first:String,second:String,lengths:Array<IntArray>) :Int
+fun stringComparator(first:String,second: String, requiredMatch: Double = 2.0/*max()*/): Boolean{
+   val lcsLength: Double=lcsLength(first,second);
+   return (lcsLength/first.length+lcsLength/second.length) >= requiredMatch
+}
+
+fun lcsLength(first:String,second:String) :Double
 {
+   var lengths=Array(first.length+1) { IntArray(second.length+1) }
    for (i in first.length downTo 0)
       for (j in second.length downTo 0)
       {
@@ -15,15 +21,16 @@ fun lcsLength(first:String,second:String,lengths:Array<IntArray>) :Int
          else if (first[i]==second[j]) lengths[i][j]=1+lengths[i+1][j+1]
          else lengths[i][j] = max(lengths[i+1][j],lengths[i][j+1])
       }
-   return lengths[0][0]
+   return lengths[0][0].toDouble()
 }
 
-fun /*longestCommonPartOfFile*/littoralCombatShip(baseText:List<String>, resultText:List<String>, lengths:Array<IntArray>) : Int{
+fun /*longestCommonGroupOfStrings*/littoralCombatShip(baseText:List<String>, resultText:List<String>,requiredMatch :Double=2.0) : Int{
+   var lengths=Array(baseText.size+1) { IntArray(resultText.size+1) }
    for (i in baseText.size downTo 0)
       for (j in resultText.size downTo 0)
       {
          if (i==baseText.size || j==resultText.size) lengths[i][j]=0
-         else if (baseText[i]==resultText[j]) lengths[i][j]=1+lengths[i+1][j+1]
+         else if (stringComparator(baseText[i],resultText[j])) lengths[i][j]=1+lengths[i+1][j+1]
          else lengths[i][j] = max(lengths[i+1][j],lengths[i][j+1])
       }
    return lengths[0][0]
@@ -32,28 +39,21 @@ fun /*longestCommonPartOfFile*/littoralCombatShip(baseText:List<String>, resultT
 
 fun main(){
 
+
    //Lcs test zone
-
-   /*var first = "abcd"
+/*
+   var first = "abcd"
    var second = "ckldr"
-   var lengths=Array(first.length+1) { IntArray(second.length+1) }
 
-   println(lcsLength(first,second, lengths))
+   println(lcsLength(first,second))
+*/
 
-   for(i in 0..lengths.size-1)
-   {
-      repeat(lengths[i].size)
-      {
-         print("${lengths[i][it]} ")
-      }
-      println()
-   }*/
 
 //------------------------------------------------------
 
    //LittoralCombatShip test zone
 
-   val baseFile = File("src\\main\\resources\\base.txt")
+ /*  val baseFile = File("src\\main\\resources\\base.txt")
    val resultFile = File("src\\main\\resources\\result.txt")
 
    val baseStrings:List<String> = baseFile.readLines()
@@ -67,11 +67,19 @@ fun main(){
       println(resultStrings[it])
    }
    val lengths=Array(baseStrings.size+1) { IntArray(resultStrings.size+1) }
-   println(littoralCombatShip(baseStrings,resultStrings, lengths))
+   println(littoralCombatShip(baseStrings,resultStrings))
 
-
+*/
 
 //------------------------------------------------------
+
+   // stringComparator test zone
+/*
+   var first = "abcdefghijk"
+   var second = "abcdefghijk"
+   stringComparator(first,second)
+*/
+   //------------------------------------------------------
    
    // Помощь в тестировании утилиты
    /*val example = "1234567890"
